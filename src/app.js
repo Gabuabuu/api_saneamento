9f5dd1a56c4efbf8ff13331e6475157895216d76
Gabuabuu <gabydavilaneves@gmail.com>
2024-05-20 02:41:03 -0300

    refactor: removing the connection from app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
-import connectionDataBase from "./app/database/connection.js";
 import managementController from "./app/controllers/management.controller.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/bairros", managementController.neighborhoodList);
 
 app.get("/bairros/buscar", managementController.neighborhoodList);
 
@@ -15,5 +16,5 @@ app.post("/bairros/gerenciamento", managementController.neighborhoodAdd);
 app.put("/bairros/gerenciamento/update/:id", managementController.neighborhoodUpdate);
 
 app.delete("/bairros/gerenciamento/delete/:id", managementController.neighborhoodDelete);
-
+ 
 export default app;
