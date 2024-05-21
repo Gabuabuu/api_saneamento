a197d5eea24a0f1ec204660f8a54526191f65e58
Gabuabuu <gabydavilaneves@gmail.com>
2024-05-21 15:11:08 -0300

    refactor:

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,10 @@
-import express from "express";
+// import express from "express";
 
-const app = express()
+// const app = express()
 
-app.get('/', (req, res) => {
-    res.send({message: "Rota raiz"})
-    console.log({ message: "Rodando servidor" })
-})
+// app.get('/', (req, res) => {
+//     res.send({message: "Rota raiz"})
+//     console.log({ message: "Rodando servidor" })
+// })
 
-export default app
+// export default app
